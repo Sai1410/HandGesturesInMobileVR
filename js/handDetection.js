c55d0b075eb941bed16a52a3d527e1a5f30860e4
Sai1410 <34296812+Sai1410@users.noreply.github.com>
2018-06-15 03:42:40 +0200

    Added detection of fingertips

--- js/handDetection.js
+++ js/handDetection.js
@@ -8,41 +8,24 @@ let temp4 = new cv.Mat();
 let dst = new cv.Mat.zeros(video.height, video.width, cv.CV_8UC3);
 let cap = new cv.VideoCapture(video);
 
-
-let lowScalarHLS = new cv.Scalar(0, 0.1 * 255, 0.05 * 255);
-let highScalarHLS = new cv.Scalar(15, 0.8 * 255, 0.6 * 255);
+let color = new cv.Scalar(255, 0, 0);
 
 let lowScalarHSV = new cv.Scalar(90, 0, 0);
 let highScalarHSV = new cv.Scalar(140,255, 0.5 * 255);
 
+var mainTips;
 
-let lowScalarRGB = [117, 117, 117, 0];
-let highScalarRGB = [255, 255, 255, 255];
+let tipPoints = [];
 
+function getDist(a, b){
 
-function makeHandMaskRGBA(){
-
-		let low = new cv.Mat(src.rows, src.cols, src.type(), lowScalarRGB);
-		let high = new cv.Mat(src.rows, src.cols, src.type(), highScalarRGB);
-		cv.inRange(src,low, high,temp);
-
-		let ksize = new cv.Size(9, 9);
-		cv.GaussianBlur(temp, temp, ksize, 0, 0, cv.BORDER_DEFAULT);
-		cv.threshold(temp, temp, 200, 255,cv.THRESH_BINARY);
+	let diffx = a.x-b.x;
+	let diffy = a.y-b.y;
+	
+	return Math.sqrt(diffx*diffx + diffy*diffy);
 }
 
-function makeHandMaskGRAY(){
 
-		cv.cvtColor(src, temp, cv.COLOR_RGB2GRAY);
-
-		let low = new cv.Mat(temp.rows, temp.cols, temp.type(), lowScalarRGB);
-		let high = new cv.Mat(temp.rows, temp.cols, temp.type(), highScalarRGB);
-		cv.inRange(temp,low, high,temp);
-
-		let ksize = new cv.Size(9, 9);
-		cv.GaussianBlur(temp, temp, ksize, 0, 0, cv.BORDER_DEFAULT);
-		cv.threshold(temp, temp, 200, 255,cv.THRESH_BINARY);
-}
 
 function makeHandMaskHSV(){
 
@@ -57,31 +40,26 @@ function makeHandMaskHSV(){
 		
 }
 
-function makeHandMaskHLS(){
+function findCenter(cnt){
 
-		cv.cvtColor(src, temp, cv.COLOR_BGR2HLS);
-		let low = new cv.Mat(temp.rows, temp.cols, temp.type(), lowScalarHLS);
-		let high = new cv.Mat(temp.rows, temp.cols, temp.type(), highScalarHLS);
-		cv.inRange(temp,low, high,temp);
-		let ksize = new cv.Size(9, 9);
-		cv.GaussianBlur(temp, temp, ksize, 0, 0, cv.BORDER_DEFAULT);
-		cv.threshold(temp, temp, 200, 255,cv.THRESH_BINARY);
-		
+	let moments = cv.moments(cnt, false);
+	let centroid = new cv.Point();
+	if(moments.m00 !=0 ){
+	
+		centroid.x = moments.m10/moments.m00;
+		centroid.y = moments.m01/moments.m00;
+	
+	}
+	return centroid;
 }
 
-function makeContours(){
+function getLargestContour(contours){
 
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-	let color = new cv.Scalar(255, 0, 0);
-	let max_contour_index = 0; 
-	let cnt;
-	
-	cv.findContours(temp, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
 	let max_area = 0;
-
-	// get largest contour
-
+	let cnt;
+	let area;
+	let max_contour_index = 0; 
+	
 	for (let i = 0; i < contours.size(); ++i) {
             cnt=contours.get(i);
             area = cv.contourArea(cnt, false)
@@ -90,29 +68,127 @@ function makeContours(){
                 max_contour_index=i;
             }
 	}
-	
+
 	cnt = contours.get(max_contour_index);
-	if(cnt){
+
+	return cnt;
+}
+
+function getRectangle(cnt){
+
+	return cv.boundingRect(cnt);
+
+}
+
+function decideTipPoint(previewTip, point, rect){
 	
-			
-		let tmp = new cv.Mat();
-		
-		let hull = new cv.MatVector();
-		
-		
-		cv.convexHull(cnt, tmp, false, true);
-		hull.push_back(tmp);
-		cv.drawContours(dst, hull, 0, color, 2, cv.LINE_8, hierarchy, 0);
+	dist = getDist(new cv.Point(point.x, rect.y + rect.height), point);
+	if(previewTip){
+	
+		dist2 = getDist(previewTip, point);
+		if(dist2 > 20){
+			if(dist > (rect.height / 4)){
+				tipPoints.push(point);
+			}
+		} 
+	} else {
+		if(dist > (rect.height / 4)){
+			tipPoints.push(point);
+		}
 	}
 
+}
 
 
+function detectFingerTips(cnt, hull, rect){
+	
+	let defect = new cv.Mat();
+	let total = 0;
+	let previewTip = null;
+	let previewFold = null;
 	
 	
+	// Get all convexity defects
+	cv.convexityDefects(cnt, hull, defect);
 	
-	//cv.drawContours(dst, contours, max_contour_index, color, 2, cv.LINE_8, hierarchy, 100);
-	//return contours.get(max_contour_index);
+	for(let i = 0; i < defect.rows; i = ++i){
+	
+		let point = new cv.Point(cnt.data32S[defect.data32S[i * 4] * 2],
+			                     cnt.data32S[defect.data32S[i * 4] * 2 + 1]);
+		
+		decideTipPoint(previewTip, point, rect);
+		
+		previewTip = point;
+	}
+}
+
+function getThumbAndPointer(){
+
+	let thumb = new cv.Point();
+	let pointer = new cv.Point();
+	let minx = video.width;
+	
+	for(let i = 0; i < tipPoints.length; i++){
+	
+		if(tipPoints[i].x < minx){
+			pointer.x = thumb.x;
+			pointer.y = thumb.y;
+			thumb = tipPoints[i];	
+			minx = thumb.x;
+		} 
+	}
+	cv.circle(dst, thumb, 3, color, -1);
+	cv.circle(dst, pointer, 3, color, -1);
+}
+
+function makeContours(){
+
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+	
+	let cnt;
+	let rect;
+	let center;
+	let hull = new cv.Mat();
+	
+
+	// find contours on img
+	cv.findContours(temp, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+	// find largest contour
+	cnt = getLargestContour(contours);
+
+	// if it is not null
+	if(cnt){
+	
+		// get center of contour
+		center = findCenter(cnt);
+		cv.circle(dst, center, 3, color, -1);
+		
+		// get rectangle of contour
+		rect = getRectangle(cnt);
+		
+		// get hull
+		cv.convexHull(cnt, hull, false, false);
+		
+		// detect fingertips
+		detectFingerTips(cnt, hull, rect);
+		
+		// Get thumb and pointing finger
+		getThumbAndPointer();
+		
+		
+		tipPoints = [];
+
+	}
+
 };
+
+function detectFingers(){
+
+	
+
+}
 
 const FPS = 20;
 function processVideo() {
@@ -127,7 +203,7 @@ function processVideo() {
 		
 		makeContours();
 		
-		//detectFingers(contour);
+		detectFingers();
 		
 		//cv.cvtColor(src, temp2, cv.COLOR_BGR2HSV);
 		
