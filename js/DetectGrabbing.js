eded783bf0ec2007cddfba23eb9341d566cc6488
PSidor <p.sidor.x@gmail.com>
2018-09-16 00:49:17 +0200

    Fixed issue with memory on mobiles

--- js/DetectGrabbing.js
+++ js/DetectGrabbing.js
@@ -1,35 +1,34 @@
+let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+let temp = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+let cap = new cv.VideoCapture(video);
+
+let color = new cv.Scalar(255, 0, 0);
+
+let lowScalarHSV= new cv.Scalar(0, 0, 0);
+let highScalarHSV= new cv.Scalar(65,255, 15);
+let lowRangeHSV = new cv.Mat(temp.rows, temp.cols, temp.type(), new cv.Scalar(0, 0, 0));
+let highRangeHSV = new cv.Mat(temp.rows, temp.cols, temp.type(), new cv.Scalar(65,255, 15));
+
+let lowScalarGRAY= new cv.Scalar(0, 0, 0);
+let highScalarGRAY= new cv.Scalar(65,255, 15);
+let lowRangeGRAY= new cv.Mat(temp.rows, temp.cols, temp.type(), lowScalarGRAY);
+let highRangeGRAY = new cv.Mat(temp.rows, temp.cols, temp.type(), highScalarGRAY );
+
+let ksize = new cv.Size(9, 9);
+
+let centroid = new cv.Point();
+
+let hull = new cv.Mat();
+
+var thumb = new cv.Point();
+var pointer = new cv.Point();
+
+let contours = new cv.MatVector();
+let hierarchy = new cv.Mat();
+
+let defect = new cv.Mat();
+
 function DetectGrabbing(video){
-	
-	let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-	let temp = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-	let dst = new cv.Mat.zeros(video.height, video.width, cv.CV_8UC3);
-	let cap = new cv.VideoCapture(video);
-	
-	let color = new cv.Scalar(255, 0, 0);
-
-	let lowScalarHSV= new cv.Scalar(0, 0, 0);
-	let highScalarHSV= new cv.Scalar(65,255, 15);
-	let lowRangeHSV = new cv.Mat(temp.rows, temp.cols, temp.type(), new cv.Scalar(0, 0, 0));
-	let highRangeHSV = new cv.Mat(temp.rows, temp.cols, temp.type(), new cv.Scalar(65,255, 15));
-
-	let lowScalarGRAY= new cv.Scalar(0, 0, 0);
-	let highScalarGRAY= new cv.Scalar(65,255, 15);
-	let lowRangeGRAY= new cv.Mat(temp.rows, temp.cols, temp.type(), lowScalarGRAY);
-	let highRangeGRAY = new cv.Mat(temp.rows, temp.cols, temp.type(), highScalarGRAY );
-
-	let ksize = new cv.Size(9, 9);
-
-	let centroid = new cv.Point();
-
-	let hull = new cv.Mat();
-
-	var thumb = new cv.Point();
-	var pointer = new cv.Point();
-
-	let contours = new cv.MatVector();
-	let hierarchy = new cv.Mat();
-
-	let defect = new cv.Mat();
 
 	var grabState = false;
 
@@ -63,8 +62,6 @@ function DetectGrabbing(video){
 		
 		// detect fingertips
 		detectFingerTips(cnt, hull, rect, defect, tipPoints);
-		
-		console.log(tipPoints.length)
 		
 		if(tipPoints.length > 3){
 			
