705c3d9f4b69a00f77a413ef328b835afd2ded9b
Sai1410 <p.sidor.x@gmail.com>
2019-03-30 05:31:34 +0100

    Code Refactoring

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 var hovered = false;
 
-var grabState = false;
-
 var zero_position = true;
 var d = 0;
 var el = document.getElementById('box');
@@ -14,66 +12,66 @@ let begin = Date.now();
 let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
 let start_camera_rotation = cam.getAttribute('rotation');
 let alfa;
+
+let browser = "Firefox"
+let mode = "Main"
+//Available modes:  HandMask, Contours, LargestContour, Rectangle, Hull, FingerTips, ThumbAndPointer, NoDetection
+
+function setDefaultCursorColor(){
+	zero_position=true;
+	cur.setAttribute('color', defaultCursorColor )
+}
+
 function checkHover() {
+	el.addEventListener('mouseenter', function () {
+		hovered = true;
+		el.setAttribute('color', "blue");
+	});
+	el.addEventListener('mouseleave', function () {
+		hovered = false;
+		el.setAttribute('color', "white");
+	});	  
+}
 
-	  el.addEventListener('mouseenter', function () {
-	  		hovered = true;
-			el.setAttribute('color', "blue");
-	  });
+function browserDetection() {
+    if (navigator.userAgent.search("Chrome")) {
+        browser="Chrome"
+    } else if (navigator.userAgent.search("Chromium")){
+		browser="Chromium"
+	}
+}
 
-	  el.addEventListener('mouseleave', function () {
-		  	hovered = false;
-			el.setAttribute('color', "white");
-	  });
-	  
+function setObjectStartPositions(curr_camera_position, curr_camera_rotation) {
+	let curr_obj_position = el.getAttribute('position');
+		
+	start_obj_point = new cv.Point(curr_obj_position.z, curr_obj_position.x);
+	start_camera_point = new cv.Point(curr_camera_position.z, curr_camera_position.x);
+	
+	start_camera_rotation = curr_camera_rotation.y;
+
+	zero_position = false;
+}
+
+function setObjectNewPositions(curr_camera_position, curr_camera_rotation){
+	deltaAngle = Math.abs(curr_camera_rotation.y - start_camera_rotation);
+		
+	// Calculate new position
+	let new_position_z = start_obj_point.x * Math.cos(toRadians(deltaAngle)) + start_obj_point.y * Math.sin(toRadians(deltaAngle));
+	let new_position_x = (-1) * start_obj_point.y * Math.cos(toRadians(deltaAngle)) + start_obj_point.x * Math.sin(toRadians(deltaAngle));
+
+	el.object3D.position.set((-1) * new_position_x, curr_camera_position.y, new_position_z);
 }
 
 function moveAnObject() {
-
-	if(hovered){
-
-		grabState = DetectGrabbing(video, true);
-		
-		
-		if(grabState){
-		  	
-			cur.setAttribute('color', 'red' )
-			
-			let curr_camera_position = cam.getAttribute('position');
-			let curr_camera_rotation = cam.getAttribute('rotation');
-			
-			if(zero_position == true){
-			
-				let curr_obj_position = el.getAttribute('position');
+	cur.setAttribute('color', 'red' )
 				
-				start_obj_point = new cv.Point(curr_obj_position.z, curr_obj_position.x);
-				start_camera_point = new cv.Point(curr_camera_position.z, curr_camera_position.x);
-				
-				start_camera_rotation = curr_camera_rotation.y;
-				// Count distance between camera and obj
-				//d = getDist(obj_point, camera_point);
-				zero_position = false;
-					
-			} else {
-			
-				alfa = Math.abs(curr_camera_rotation.y - start_camera_rotation);
-				
-				// Count new position
-				let new_position_z = start_obj_point.x * Math.cos(toRadians(alfa)) + start_obj_point.y * Math.sin(toRadians(alfa));
-				let new_position_x = (-1) * start_obj_point.y * Math.cos(toRadians(alfa)) + start_obj_point.x * Math.sin(toRadians(alfa));
-				console.log(new_position_z)
-				console.log(new_position_x)
-				el.object3D.position.set((-1) * new_position_x, curr_camera_position.y, new_position_z);
-			}
-		} else {
-			// If not grabbing object start again
-			zero_position=true;
-			cur.setAttribute('color', defaultCursorColor )
-		}
+	let curr_camera_position = cam.getAttribute('position');
+	let curr_camera_rotation = cam.getAttribute('rotation');
+	
+	if(zero_position == true){
+		setStartPositions(curr_camera_position, curr_camera_rotation)
 	} else {
-		// If not grabbing object start again
-		zero_position=true;
-		cur.setAttribute('color', defaultCursorColor )
+		setNewPositions(curr_camera_position, curr_camera_rotation)
 	}
 }
 
@@ -81,17 +79,58 @@ function toRadians (angle) {
 	  return angle * (Math.PI / 180);
 	}
 
+function mainMode(){
+
+	browserDetection();
+
+	checkHover();
+
+	if(hovered){
+		if(DetectGrabbing(video, true)){
+			moveAnObject();
+		} else {
+			setDefaultCursorColor();
+		}
+	} else {
+		setDefaultCursorColor();
+	}
+
+}
+
+function noDetectionMode() {
+	checkHover();
+}
+
 const FPS = 25;
 function processVideo() {
-        begin = Date.now();
+		begin = Date.now();
+		
 		//dst = src.clone();
-        // start processing.
-		checkHover();
-//		
-		moveAnObject();
-       // DetectGrabbing(video, true);
-        
+		
+		switch(mode){
+			case "Main":
+				mainMode();
+				break;
+			case "Contours":
+				contoursMode();
+				break;
+			case "LargestContour":
+				break;
+			case "Rectangle":
+				break;
+			case "Hull":
+				break;
+			case "FingerTips":
+				break;
+			case "ThumbAndPointer":
+				break;
+			case "NoDetection":
+				noDetectionMode();
+				break;
+			default:
 
+		}
+    
         //cv.imshow('canvasOutput', dst);
 
         let delay = 1000/FPS - (Date.now() - begin);
